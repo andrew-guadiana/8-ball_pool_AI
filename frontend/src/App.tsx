73374749ad500456d0088892a1bef71fa9d9dc1e
Andrew <andrew.guadiana@usc.edu>
2026-06-23 14:05:28 -0700

    Cue Ball is moveable by click on table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
 import { useState } from "react"
 
+type Ball = {
+  id: string
+  x: number
+  y: number
+  color: string
+}
+
 type BallProps = {
   x: number
   y: number
@@ -11,8 +18,8 @@ function Ball({ x, y, color }: BallProps) {
     <div
     style={{
       position: "absolute",
-      left: x,
-      top: y,
+      left: x - 15,
+      top: y - 15,
       width: 30,
       height: 30,
       borderRadius: "50%",
@@ -23,7 +30,7 @@ function Ball({ x, y, color }: BallProps) {
 }
 
 function App() {
-  const [balls] = useState<Ball[]>([
+  const [balls, setBalls] = useState<Ball[]>([
     {
     id: "cue",
     x: 100,
@@ -35,11 +42,25 @@ function App() {
 
 return (
   <div
+  onClick={(e: React.MouseEvent<HTMLDivElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect()
+    const x = e.clientX - rect.left
+    const y = e.clientY - rect.top
+
+    setBalls([
+      {
+        id: "cue",
+        x, 
+        y,
+        color: "white",
+      },
+    ])
+  }}
   style={{
     width: 900,
     height: 500,
+    position: "absolute", left: "center", top: "30%",
     background: "green",
-    position: "relative",
   }}>
   {balls.map(ball => (
     <Ball 
